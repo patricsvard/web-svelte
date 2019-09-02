0cbe4131f9eb939c993d743fcf2c96c231c004b5
patric.svard <patric.svard@starrepublic.com>
2019-09-02 11:51:54 +0200

    setup mdc

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
-	export let name;
+  import Button from "./button.svelte";
+  export let name;
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: purple;
+  }
 </style>
 
 <h1>Hello {name}!</h1>
+<Button />
---
+++ src/button.svelte
@@ -0,0 +1,45 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  let self = this;
+
+  export let classes = (
+    raised,
+    unelevated,
+    stroked,
+    dense,
+    compact,
+    background,
+    color
+  ) => {
+    const classList = [];
+    raised && classList.push("mdc-button--raised");
+    unelevated && classList.push("mdc-button--unelevated");
+    stroked && classList.push("mdc-button--stroked");
+    dense && classList.push("mdc-button--dense");
+    compact && classList.push("mdc-button--compact");
+    background && classList.push("mdc-theme--" + background + "-bg");
+    color && classList.push("mdc-theme--" + color);
+    return classList.join(" ");
+  };
+
+  export let text = "";
+
+  const dispatch = createEventDispatcher();
+
+  function onClick(event) {
+    dispatch("click", event);
+  }
+</script>
+
+<style type="text/scss">
+  @import "@material/button/mdc-button.scss";
+</style>
+
+<button
+  role="button"
+  bind:this={self}
+  on:click={onClick}
+  class="mdc-button {classes(true)}">
+  Hi!
+</button>
