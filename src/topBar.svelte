85f8e8d17cd2c05aa06ec2aad182bf92fa533ac3
Patric <patricsvard@gmail.com>
2019-09-03 07:58:09 +0200

    customized head storybook

--- src/topBar.svelte
+++ src/topBar.svelte
@@ -1,6 +1,5 @@
 <style type="text/scss">
   @import "./theme.scss";
-  @import "material-icons/iconfont/material-icons.scss";
   @import "@material/top-app-bar/mdc-top-app-bar";
   @import "@material/button/mdc-button";
   @import "@material/icon-button/mdc-icon-button";
@@ -11,7 +10,9 @@
     <section
       class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
       <button
-        class="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button" />
+        class="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button">
+        <i class="material-icons mdc-icon-button__icon">favorite</i>
+      </button>
       <span class="mdc-top-app-bar__title">Title</span>
     </section>
   </div>
